ccc324ec4d61a17835600da2ce2405f6fd3daa98
github username <github email address>
2020-10-14 11:12:40 -0600

    hierarchy

---
+++ src/Components/breweryCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import UserForm from './UserForm'
+
+export default function breweryCard() {
+    return (
+        <div>
+            <h3>Brewery Cards</h3>
+            <UserForm />
+        </div>
+    )
+}
---
+++ src/Components/breweryContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import BreweryCard from './breweryCard'
+
+export default function breweryContainer() {
+    return (
+        <div>
+            <h2>Brewery Container</h2>
+            <BreweryCard />
+        </div>
+    )
+}
